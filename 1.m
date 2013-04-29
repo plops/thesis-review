00776a4357d8466eda758fd67b6bfd894d81de4d
Martin Kielhorn <kielhorn.martin@googlemail.com>
2013-04-29 14:57:05 +0200

    compare hilo with other methods

--- 1.m
+++ 1.m
@@ -148,7 +148,7 @@ clear kG;
 
 tic
 struc = newim(size(otf,1),size(otf,2),size(S,3),phases);
-for slice = 0:size(S)-1
+for slice = 0:size(S,3)-1
     struc(:,:,slice,:) = create_structured_slice(S,otf,phases,slice);
     toc
 end
@@ -165,53 +165,29 @@ S_slice = S(:,:,WF_z);
 
 % different sectioning methods
 % benedetti 1996
-section_max_min = @(a) max(a,[],4)-min(a,[],4)
+section_max_min = @(a) squeeze(max(a,[],4)-min(a,[],4))
 % neil 1997 und ben-levy, peleg 1995
-section_homodyne = @(a) abs(a(:,:,:,0)+a(:,:,:,1)*exp(i*2*pi*1/4)+a(:,:,:,2)*exp(i*2*pi*2/4)+a(:,:,:,3)*exp(i*2*pi*3/4))
+section_homodyne = @(a) squeeze(abs(a(:,:,:,0)+a(:,:,:,1)*exp(i*2*pi*1/4)+a(:,:,:,2)*exp(i*2*pi*2/4)+a(:,:,:,3)*exp(i*2*pi*3/4)))
 
 % compare reconstructions of noisy images
-maxmin=section_max_min(noise(struc/max(struc)*6000,'poisson'))
-homody=section_homodyne(noise(struc/max(struc)*6000,'poisson'))
-diplink(1,[2])
+tic
+noise_struc=struc; %noise(struc/max(struc)*6000,'poisson');
+maxmin=section_max_min(noise_struc)
+homody=section_homodyne(noise_struc);
+hilo=section_hilo(noise_struc,.13,otf2d,otf2dcorr)
+toc
+diplink(1,[2 3])
 
-exp(-rr(otf2d,'freq')^2/.1^2)
+plot(double(squeeze(hilo(42,39,:))/100),'b');
+hold on
+plot(double(squeeze(homody(42,39,:))),'g');
+hold on
+plot(double(squeeze(maxmin(42,39,:))),'r');
+hold on
+plot(double(squeeze(WF(42,39,floor(size(otf,3)/2)-floor(size(S,3)/2)+(0:size(S,3)-1))))/1000,'c');
+hold off
 
-% erzeuge einen filter der im fourier raum rund ist
-% beziehe ihn auf die groesse der otf2d
-filter_fwhm = .13;
-% wo ist fwhm fuer gauss:
-% solve(exp(-x^2/sigma^2)=.5,x);
-% x=sqrt(log(2))*sigma=0.8326 sigma
-filter_sigma= filter_fwhm/sqrt(log(2));
-lowpass = extract(DampEdge(exp(-rr(otf2d)^2/(filter_sigma*size(otf2d,1))^2),.2,2,0),[size(uni,1) size(uni,2)]);
-hipass = 1-lowpass;
 
-ring = rr(otf2d)< filter_fwhm*size(otf2d,1);
-ring = extract(bdilation(ring)-ring,[size(uni,1) size(uni,2)]);
-
-uni = repmat(otf2dcorr,[1 1 size(struc,3)])*squeeze(dip_fouriertransform(struc(:,:,:,0)+struc(:,:,:,2),'forward',[1 1 0 0]));
-
-nonuni_unshifted = repmat(otf2dcorr,[1 1 size(struc,3)])*squeeze(dip_fouriertransform(struc(:,:,:,0)-struc(:,:,:,2),'forward',[1 1 0 0]));
-
-tiltbig = 2*pi*xx(size(uni,1),size(uni,2))/64*12;
-
-nonuni = dip_fouriertransform(repmat(exp(-i*tiltbig),[1 1 size(struc,3)])*dip_fouriertransform(nonuni_unshifted,'inverse',[1 1 0]),'forward',[1 1 0]);
-
-% erweitere auf 3d
-ring3= repmat(ring,[1 1 size(uni,3)]);
-lowpass3 = repmat(lowpass,[1 1 size(uni,3)]);
-hipass3 = repmat(hipass,[1 1 size(uni,3)]);
-
-ringhi = mean(ring3*abs(hipass3*uni)^2,[],[1 2]);
-ringlo = mean(ring3*abs(lowpass3*nonuni)^2,[],[1 2]);
-
-eta = ringhi/ringlo;
-
-eta_median=median(eta);
-      
-image_lo = imag(dip_fouriertransform(lowpass3 * nonuni,'inverse',[1 1 0]));
-image_hi = real(dip_fouriertransform(hipass3 * uni,'inverse',[1 1 0]));
-sec = image_lo*eta_median+image_hi;
 
 %% Local Variables:
 %% mode: Octave
---
+++ section_hilo.m
@@ -0,0 +1,39 @@
+function sec = section_hilo(struc,filter_fwhm,otf2d,otf2dcorr)
+uni = repmat(otf2dcorr,[1 1 size(struc,3)])*squeeze(dip_fouriertransform(struc(:,:,:,0)+struc(:,:,:,2),'forward',[1 1 0 0]));
+
+nonuni_unshifted = repmat(otf2dcorr,[1 1 size(struc,3)])*squeeze(dip_fouriertransform(struc(:,:,:,0)-struc(:,:,:,2),'forward',[1 1 0 0]));
+
+tiltbig = 2*pi*xx(size(uni,1),size(uni,2))/64*12;
+
+nonuni = dip_fouriertransform(repmat(exp(-i*tiltbig),[1 1 size(struc,3)])*dip_fouriertransform(nonuni_unshifted,'inverse',[1 1 0]),'forward',[1 1 0]);
+
+
+% erzeuge einen filter der im fourier raum rund ist
+% beziehe ihn auf die groesse der otf2d
+
+% wo ist fwhm fuer gauss:
+% solve(exp(-x^2/sigma^2)=.5,x);
+% x=sqrt(log(2))*sigma=0.8326 sigma
+filter_sigma= filter_fwhm/sqrt(log(2));
+lowpass = extract(DampEdge(exp(-rr(otf2d)^2/(filter_sigma*size(otf2d,1))^2),.2,2,0),[size(uni,1) size(uni,2)]);
+hipass = 1-lowpass;
+
+ring = rr(otf2d)< filter_fwhm*size(otf2d,1);
+ring = extract(bdilation(ring)-ring,[size(uni,1) size(uni,2)]);
+
+
+% erweitere auf 3d
+ring3= repmat(ring,[1 1 size(uni,3)]);
+lowpass3 = repmat(lowpass,[1 1 size(uni,3)]);
+hipass3 = repmat(hipass,[1 1 size(uni,3)]);
+
+ringhi = mean(ring3*abs(hipass3*uni)^2,[],[1 2]);
+ringlo = mean(ring3*abs(lowpass3*nonuni)^2,[],[1 2]);
+
+eta = ringhi/ringlo;
+
+eta_median=median(eta);
+      
+image_lo = imag(dip_fouriertransform(lowpass3 * nonuni,'inverse',[1 1 0]));
+image_hi = real(dip_fouriertransform(hipass3 * uni,'inverse',[1 1 0]));
+sec = image_lo*eta_median+image_hi;
